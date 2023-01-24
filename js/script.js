d39b8ce0c893ea9617dc6f505a27d7975e2474c3
Denys Kovtun <denys.kovtun@gmail.com>
2023-01-24 21:57:19 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// const age = 10;
+// const totalPrice = 228;
+// const userName = "chelsea";
+// const isOpen = true;
+// const message = "Поновити облік фрагів?"
+// const x = 5;
+// let c = 5;
+// c = 10;
+// console.log("Wtf", totalPrice);
+// const type = typeof true;
+// console.log(type);
+// alert('fuck yourself beach');
+// const shouldRenew = confirm(message);
+// const quantity = prompt("Введите ваш возраст?");
+// console.log(quantity);
+
+// console.log(Math.PI);
+
+// const base = 4;
+// const power = 5;
+
+// const result = Math.pow(base, power)
+// console.log(result);
+// console.log(2 ** 8);
+// const square = Math.sqrt(255);
+// console.log(square);
+
+// let base = prompt("Введите число");
+// base = Number(base);
+// console.log(base);
+// let power = prompt("Введите степень")
+// power = Number(power);
+// console.log(power);
+// const result = base ** power;
+// console.log(result);
+
+// const result = Math.round(Math.random()*(100-50)+50);
+// console.log(result);
