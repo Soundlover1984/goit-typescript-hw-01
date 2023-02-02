281532514fec30abdc944550f5007cb1e847f124
Soundlover1984 <denys.kovtun@gmail.com>
2023-02-02 11:40:20 +0200

    Final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+// let elementWidth = "60.29px";
+// elementWidth = Number.parseInt(elementWidth);
+// console.log ('Довжина елементу:', elementWidth);
+
+// let elementHeight = "240.49px";
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log ('Висота елементу:', elementHeight);
+
+
+// let salary = 1263.444567;
+// salary = salary.toFixed(2);
+// salary = Number(salary);
+// console.log ("Ваша зарплата:", salary);
+
+// let quautity = prompt("Введите значение переменной");
+// quautity = Number (quautity);
+// console.log (quautity);
+// console.log (typeof quautity);
+
+// let base = prompt("Введите число");
+// console.log(Number(base));
+// const power = prompt("Введите степень")
+// console.log(Number(power));
+// const result = base ** power;
+// console.log(result);
+
+
+// const message = "Когда закончится война в Украине"
+// console.log(message.length);
+
+// console.log(`Привіт мене звати Денис. Мені ${30-8} років`);
+
 // const age = 10;
 // const totalPrice = 228;
 // const userName = "chelsea";
@@ -36,3 +68,4 @@
 
 // const result = Math.round(Math.random()*(100-50)+50);
 // console.log(result);
+
